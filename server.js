036ba4f75d8ae60e235d0a4f9a6b070eb3368c0e
SEHacker <69587550+pozz-seang@users.noreply.github.com>
2023-07-20 16:11:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.use(cors({
     origin: [
         "https://jstool.netlify.app",
         "https://jstools.co",
-        "http://jstools.co",
         "http://192.168.1.11:3000"
     ],
     credentials: true,
