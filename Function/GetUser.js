c85c21f4623ba2f7e9e9b50eadbe05c48bda6933
SEHacker <69587550+pozz-seang@users.noreply.github.com>
2023-07-20 23:06:58 +0700

    Update GetUser.js

--- Function/GetUser.js
+++ Function/GetUser.js
@@ -4,6 +4,7 @@ const { checkExpired } = require("./Date");
 const GetUser = async (req, res) => {
     const { data, error } = await DB.from(DB_User).select(`id, username, email, expired, pe_image, facebook, cta_option, plan, role`).eq("email", req.user.email)
     if (error) return res.json({ error: "Error" });
+    if(!data[0]) return  res.json({ code: 165, msg:"user expired sesstion" });
     data[0].expiredStatus = !checkExpired(data[0].expired)
     res.json(data[0])
 }
